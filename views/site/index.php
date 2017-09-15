8901454adfb7f2a8e699e81cdc5b364e79e98475
aditseller <tezaraditya@gmail.com>
2017-09-15 17:41:08 +0700

    upload

--- views/site/index.php
+++ views/site/index.php
@@ -1,5 +1,5 @@
 <?php
-use ckarjun\owlcarousel\OwlCarouselWidget;
+use kv4nt\owlcarousel\OwlCarouselWidget;
 /* @var $this yii\web\View */
 
 $this->title = Yii::$app->params['siteName'];
@@ -8,7 +8,7 @@ $this->title = Yii::$app->params['siteName'];
 
    <div>
       <div id="myCarousel" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
+		 <!-- Indicators -->
   <ol class="carousel-indicators">
     <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
     <li data-target="#myCarousel" data-slide-to="1"></li>
@@ -20,7 +20,7 @@ $this->title = Yii::$app->params['siteName'];
     <div class="item active">
       <img src="https://www.jakartanotebook.com/images/banners/new-arrival-skmei.jpg" alt="Chania">
       <div class="carousel-caption">
-	  Bla Bla Bla
+	  Nggak Cuma Bergaya Keren, 5 Model Dunia Ini Juga Jago Motret!
       </div>
     </div>
 
@@ -57,16 +57,16 @@ $this->title = Yii::$app->params['siteName'];
 			
 			<tr style="border-bottom:1px solid #b302b5;">
 			<td width="70%">
-			<p style="color:Turquoise; font-size:1.0em; font-family:Arial Black;">Workout</p>
-			<p style="color:#b302b5; font-size:1.0em; font-family:Arial; font-weight:bold;">Ketahui Betapa Spesial-nya Dirimu Lewat 5 Hal Ini</p>
+			<p style="color:Turquoise; font-size:1.0em; font-family:Arial Black; margin-left:5px;">Workout</p>
+			<p style="color:#b302b5; font-size:1.0em; font-family:Arial; font-weight:bold; margin-left:5px;">Ketahui Betapa Spesial-nya Dirimu Lewat 5 Hal Ini</p>
 			</td>
 			<td width="30%"><img width="100%" src="https://cdn.popbela.com/content-images/post/20170904/18382344-1777277315860908-2778590244168007680-n-f1837c0e793e3feb6b91a0997f28ab26_390x260.jpg"></td>
 			</tr>
 			
 			<tr style="border-bottom:1px solid #b302b5;">
 			<td width="70%">
-			<p style="color:Turquoise; font-size:1.0em; font-family:Arial Black;">Inspiration</p>
-			<p style="color:#b302b5; font-size:1.0em; font-family:Arial; font-weight:bold;">Nggak Cuma Bergaya Keren, 5 Model Dunia Ini Juga Jago Motret!</p>
+			<p style="color:Turquoise; font-size:1.0em; font-family:Arial Black; margin-left:5px;">Inspiration</p>
+			<p style="color:#b302b5; font-size:1.0em; font-family:Arial; font-weight:bold; margin-left:5px;">Nggak Cuma Bergaya Keren, 5 Model Dunia Ini Juga Jago Motret!</p>
 			</td>
 			<td width="30%"><img width="100%" src="https://cdn.popbela.com/content-images/post/20170911/4931031f2d8e-243a09312c9d31a83df63c28e7576679_750x500.jpg"></td>
 			</tr>
@@ -75,7 +75,33 @@ $this->title = Yii::$app->params['siteName'];
              </div>
 			 
 			 <div>
-			
+			<?php
+
+
+OwlCarouselWidget::begin([
+    'container' => 'div',
+    'containerOptions' => [
+        'id' => 'container-id',
+        'class' => 'container-class'
+    ],
+    'pluginOptions'    => [
+        'autoplay'          => false,
+        'autoplayTimeout'   => 3000,
+        'items'             => 2,
+        'loop'              => true,
+        'itemsDesktop'      => [1199, 3],
+        'itemsDesktopSmall' => [979, 3]
+    ]
+]);
+?>
+
+<div class="item-class"><img src="https://cdn.popbela.com/content-images/post/20160209/fanpop-3bb88d192b155d91e6a42b58bedf9f54_200x200.jpg" alt="Image 1"></div>
+<div class="item-class"><img src="https://cdn.popbela.com/content-images/post/20160826/8-patricksponauglecom-ebce34e82dfad927d3c17b439c8c6d3a_200x200.jpg" alt="Image 2"></div>
+<div class="item-class"><img src="https://cdn.popbela.com/content-images/post/20160204/magic4wallscom-ad4d0df980402ec5cc00dd9d8a38dcf5_200x200.jpg" alt="Image 3"></div>
+
+
+
+<?php OwlCarouselWidget::end(); ?>
 			 </div>
             
             
